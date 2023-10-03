7b58ad5ae02b7373d77ea33d16aa56791d642a47
Naz <nazanin.mohammadi01@gmail.com>
2023-10-03 09:28:30 -0700

    Updates

--- index.php
+++ index.php
@@ -4,12 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/main.css">
-    <title>Snake Game</title>
+    <title>Snake Safari</title>
 </head>
 <body>
-    <h1> Welcome to my snake game bitches🐍 </h1>
+    <h1> Welcome to my snake game 🐍</h1>
     <div class="scoreDisplay"></div>
-    <div class="grid"></div>
+    <div class="grid">
+        <!-- <img src="assets/apple.png" alt="Apple" id="apple"> -->
+    </div>
+
     <div class="button">
         <button class="top">Top</button>
         <button class="bottom">Bottom</button>
